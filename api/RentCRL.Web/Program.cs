4df7202d71f4b2ea9a648251f56ed51cc60e2c96
MickaelCRL <ce.mickael27@gmail.com>
2025-01-19 02:58:33 +0100

    Set up communication between app and api

--- api/RentCRL.Web/Program.cs
+++ api/RentCRL.Web/Program.cs
@@ -1,8 +1,23 @@
+const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials();
+                      });
+});
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
@@ -13,6 +28,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 var summaries = new[]
 {
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState<any[]>();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:5047/weatherforecast");
+      const data = (await res.json()) as any[];
+      setResult(data);
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Resultats</h1>
+
+      {result?.map((el, index) =>
+        el ? (
+          <div key={index}>
+            <h2>{el.date}</h2>
+            <h3>{el.temperatureC}</h3>
+            <h3>{el.temperatureF}</h3>
+            <h3>{el.summary}</h3>
+          </div>
+        ) : (
+          <div>null</div>
+        )
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
