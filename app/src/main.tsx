56ed175938dfce14ffd48ab5d993b32698c988cc
MickaelCRL <ce.mickael27@gmail.com>
2025-01-20 23:07:58 +0100

    Add authentification with auth0

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,7 +1,16 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
+import { Auth0Provider } from "@auth0/auth0-react";
 import router from "./router";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <Auth0Provider
+    domain="dev-bko6t72qud7vo3ue.us.auth0.com"
+    clientId="UGNkulDhA0DnTQWwQ0E42J5XMf1aDlAo"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <RouterProvider router={router} />
+  </Auth0Provider>
 );
