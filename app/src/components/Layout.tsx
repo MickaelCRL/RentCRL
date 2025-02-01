59d9d0999fbaf01b4fcfb315ffabeedd8e8570a3
Mickaël Céraline <101481137+MickaelCRL@users.noreply.github.com>
2025-02-01 13:33:18 +0100

    Update Layout.tsx

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -2,18 +2,8 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <>
-      <section
-        id="section"
-        style={{
-          width: "100%",
-          height: "70vh",
-          alignItems: "center",
-          margin: "100px 0",
-        }}
-      >
-        <Outlet />
-      </section>
+    <>     
+      <Outlet />    
     </>
   );
 };
