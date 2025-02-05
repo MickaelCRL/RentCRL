5f394dcf69b5d31f753163f79ad8effa236a8edd
MickaelCRL <ce.mickael27@gmail.com>
2025-02-05 22:54:19 +0100

    Validate property of owner

--- api/RentCRL.Domain/Owner.cs
+++ api/RentCRL.Domain/Owner.cs
@@ -11,6 +11,21 @@
 
         public Owner(string auth0Id, string firstName, string lastName, string email, string phoneNumber)
         {
+            if (string.IsNullOrEmpty(auth0Id))
+                throw new ArgumentException("Auth0Id cannot be null.", nameof(auth0Id));
+
+            if (string.IsNullOrEmpty(firstName))
+                throw new ArgumentException("Firstname cannot be null.", nameof(firstName));
+
+            if (string.IsNullOrEmpty(lastName))
+                throw new ArgumentException("LastName cannot be null.", nameof(lastName));
+
+            if (!Regexes.Email.IsMatch(email))
+                throw new ArgumentException("Email is not valid.", nameof(email));
+
+            if (!Regexes.PhoneNumber.IsMatch(phoneNumber))
+                throw new ArgumentException("PhoneNumber is not valid.", nameof(phoneNumber));
+
             Id = Guid.NewGuid();
             Auth0Id = auth0Id;
             FirstName = firstName;
